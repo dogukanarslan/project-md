e3391f9ef24a8a0a74953aa754627bdd352d1fd0
Dogukan Arslan <dogukan2arslan@hotmail.com>
2024-09-28 00:10:39 +0300

    Add project component

--- app/page.js
+++ app/page.js
@@ -1,6 +1,24 @@
 import styles from "@/app/page.module.css"
-
+import Project from "@/components/Project"
 
 export default function Home() {
-  return <div className={styles.page}>Home</div>
+  return (
+    <div className={styles.page}>
+      <Project
+        title="Project Title"
+        date="March 12, 2024"
+        description="Project description"
+      />
+      <Project
+        title="Project Title"
+        date="March 12, 2024"
+        description="Project description"
+      />
+      <Project
+        title="Project Title"
+        date="March 12, 2024"
+        description="Project description"
+      />
+    </div>
+  )
 }
---
+++ components/Project.js
@@ -0,0 +1,17 @@
+import Link from "next/link"
+
+const Project = (props) => {
+  const { title, description, date } = props
+  return (
+    <div className="card">
+      <div className="project-date">Posted on {date}</div>
+      <h3>{title}</h3>
+      <p>{description}</p>
+      <Link href={`/${title}`} className="btn">
+        Read More
+      </Link>
+    </div>
+  )
+}
+
+export default Project
