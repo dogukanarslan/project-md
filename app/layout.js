0cf8d0f96cdaf8a4a58cb1df133770ff102ff76b
Dogukan Arslan <dogukan2arslan@hotmail.com>
2024-09-27 23:07:42 +0300

    Add header

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import localFont from "next/font/local"
 import "./globals.css"
+import Header from "@/components/Header"
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -20,7 +21,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
+        <Header />
+        <main className="container">{children}</main>
       </body>
     </html>
   )
