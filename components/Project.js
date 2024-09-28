9d6a335a4f8c685ad39aa82fcb7c51a931f62781
Dogukan Arslan <dogukan2arslan@hotmail.com>
2024-09-28 20:17:33 +0300

    Add image to project

--- components/Project.js
+++ components/Project.js
@@ -1,9 +1,10 @@
 import Link from "next/link"
 
 const Project = (props) => {
-  const { title, description, date } = props
+  const { title, description, date, imageUrl } = props
   return (
     <div className="card">
+      <img src={imageUrl} />
       <div className="project-date">Posted on {date}</div>
       <h3>{title}</h3>
       <p>{description}</p>
