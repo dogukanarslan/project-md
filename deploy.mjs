191bcd5125b34b3f8871bc13c206fb0e498e6b11
Dogukan Arslan <dogukan2arslan@hotmail.com>
2024-10-07 20:17:18 +0300

    Overwrite deploy history

--- deploy.mjs
+++ deploy.mjs
@@ -2,6 +2,11 @@ import ghpages from "gh-pages"
 
 ghpages.publish(
   "out",
-  { nojekyll: true, message: "Auto-generated commit", branch: "deploy" },
+  {
+    nojekyll: true,
+    message: "Auto-generated commit",
+    branch: "deploy",
+    history: false
+  },
   (err) => console.error("err", err)
 )
